a57471a09a35af52ef1e40b5952adc0308359ace
marxb <marxbacungan@gmail.com>
2021-12-27 14:20:06 -0500

    Added Weather

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import random
 import string
+import logging
+import pyowm
+from pyowm import OWM
+import requests, json
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s: %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S')
+logging.info("Logging activity for DiceRoll...")
 
 
 # Dice i sided die roll
-def dice_roll():
-    print("ROLLING SIX SIDED DIE...  ")
-    print(random.randint(1, 6))
+def dice_roll(die_num, die_num_side):
+    count = 0
+    print("ROLLING SIX SIDED DICE...  ")
+    while count < int(die_num):
+        print(random.randint(1, int(die_num_side)))
+        count = count + 1
 
 
 # Generate a random password
@@ -29,6 +44,7 @@ def binary_search(arr, low, high, x):
         return -1
 
 
+# Checks if the element is in the array, and if so, prints the index
 def print_binary_search_result(search_result):
     if search_result != -1:
         print("Element is present at index", str(search_result))
@@ -36,16 +52,49 @@ def print_binary_search_result(search_result):
         print("Element is not present in array")
 
 
+def get_weather():
+    api_key = "e73cc6d6927a19e138e5d9f3349ebca9"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = input("Enter city name : ")
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_pressure = y["pressure"]
+        current_humidity = y["humidity"]
+        z = x["weather"]
+        weather_description = z[0]["description"]
+        print(" Temperature (in kelvin unit) = " +
+              str(current_temperature) +
+              "\n atmospheric pressure (in hPa unit) = " +
+              str(current_pressure) +
+              "\n humidity (in percentage) = " +
+              str(current_humidity) +
+              "\n description = " +
+              str(weather_description))
+
+    else:
+        print(" City Not Found ")
+
+
 # Roll a die
-dice_roll()
+logging.info("See how many side is the die")
+num_die_side = input("How many sided die? ")
+logging.info("See how many die is going to be rolled")
+num_dice = input("How many die you want to roll? ")
+dice_roll(num_dice, num_die_side)
 
 # Generate a random password
 generate_password()
 
 # Test array
-arr = [2, 3, 4, 10, 40]
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 x = 10
 
 # Function call
 result = binary_search(arr, 0, len(arr) - 1, x)
 print_binary_search_result(result)
+
+get_weather()
